cc3d2fbd636f9d8aaed3fc1c17c3b9228e389d2e
Michelle Mabuyo <michelle@apollographql.com>
2023-04-05 13:03:10 -0400

    starter code - pass cache

--- server/src/index.js
+++ server/src/index.js
@@ -11,9 +11,10 @@ async function startApolloServer() {
 
   const { url } = await startStandaloneServer(server, {
     context: async () => {
+      const { cache } = server;
       return {
         dataSources: {
-          trackAPI: new TrackAPI(),
+          trackAPI: new TrackAPI({ cache }),
         },
       };
     },
