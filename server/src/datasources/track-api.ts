3685105794a6b9a5e7f2b75f33b4a1a10679932b
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-07-10 21:01:56 -0600

    update starter and final code with correct data source model types

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,22 +1,23 @@
 import { RESTDataSource } from '@apollo/datasource-rest';
+import { TrackModel, AuthorModel, ModuleModel } from '../models'
 
 export class TrackAPI extends RESTDataSource {
   // the Catstronauts catalog is hosted on this server
   baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
 
   getTracksForHome() {
-    return this.get('tracks');
+    return this.get<TrackModel[]>('tracks');
   }
 
-  getAuthor(authorId) {
-    return this.get(`author/${authorId}`);
+  getAuthor(authorId: string) {
+    return this.get<AuthorModel>(`author/${authorId}`);
   }
 
-  getTrack(trackId) {
-    return this.get(`track/${trackId}`);
+  getTrack(trackId: string) {
+    return this.get<TrackModel>(`track/${trackId}`);
   }
 
-  getTrackModules(trackId) {
-    return this.get(`track/${trackId}/modules`);
+  getTrackModules(trackId: string) {
+    return this.get<ModuleModel[]>(`track/${trackId}/modules`);
   }
 }
